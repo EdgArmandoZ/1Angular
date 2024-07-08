bb539796757e509821ca1b9947e1cb16f21a0d40
Edgarwebb <161100457+Edgarwebb@users.noreply.github.com>
2024-07-08 12:35:06 -0600

    source code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Formulario7Component } from './formulario7/formulario7.component';
+import { RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Formulario7Component],
+  imports: [Formulario7Component, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
